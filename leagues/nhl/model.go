03bb1955c3fe676bdaab9139fbc30e9867daf2f7
Marko Kujala <mm.kujala@gmail.com>
2019-01-29 23:26:12 +0200

    removed omitempty from nhl models ot etc fields

--- leagues/nhl/model.go
+++ leagues/nhl/model.go
@@ -8,20 +8,20 @@ type Stats struct {
 	Team          string        `json:"team" bson:"team"`
 	Season        int           `json:"season" bson:"season"`
 	League        string        `json:"league" bson:"league"`
-	Conference    string        `json:"conference,omitempty" bson:"conference,omitempty"`
+	Conference    string        `json:"conference" bson:"conference"`
 	Venue         string        `json:"venue" bson:"venue"`
 	GP            int           `json:"gp" bson:"gp"`
 	Wins          int           `json:"wins" bson:"wins"`
 	Loses         int           `json:"loses" bson:"loses"`
-	OTLoses       int           `json:"otLoses,omitempty" bson:"otLoses,omitempty"`
-	OTWins        int           `json:"otWins,omitempty" bson:"otWins,omitempty"`
-	StrWinPercent float64       `json:"strWin_p,omitempty" bson:"strWin_p,omitempty"`
-	WinPercent    float64       `json:"win_p,omitempty" bson:"win_p,omitempty"`
-	OTpercent     float64       `json:"ot_p,omitempty" bson:"ot_p,omitempty"`
+	OTLoses       int           `json:"otLoses" bson:"otLoses"`
+	OTWins        int           `json:"otWins" bson:"otWins"`
+	StrWinPercent float64       `json:"strWin_p" bson:"strWin_p"`
+	WinPercent    float64       `json:"win_p" bson:"win_p"`
+	OTpercent     float64       `json:"ot_p" bson:"ot_p"`
 	GA            int           `json:"ga" bson:"ga"`
 	GF            int           `json:"gf" bson:"gf"`
 	GAavg         float64       `json:"gaAvg" bson:"gaAvg"`
 	GFavg         float64       `json:"gfAvg" bson:"gfAvg"`
 	PTS           int           `json:"pts" bson:"pts"`
-	PtsAvg        float64       `json:"ptsAvg,omitempty" bson:"ptsAvg,omitempty"`
+	PtsAvg        float64       `json:"ptsAvg" bson:"ptsAvg"`
 }
